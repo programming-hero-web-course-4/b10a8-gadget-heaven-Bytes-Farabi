1a42c49eb3b04496aa7af866bdcba87413adb919
Bytes-Farabi <sumaiyasyl2009@gmail.com>
2024-11-03 23:50:17 +0600

    banner done

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -10,7 +10,9 @@ const Root = () => {
             <Navbar></Navbar>
             </div>
             <Outlet></Outlet>
-            <Footer></Footer>
+            <div className='mt-40'>
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
