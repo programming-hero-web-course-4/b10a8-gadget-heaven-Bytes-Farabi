849c9d55c287e20b2b99500917ab6a3dd239fa37
Bytes-Farabi <sumaiyasyl2009@gmail.com>
2024-11-03 23:43:16 +0600

    banner relative section done(not fully)

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <section className="bg-purple-600 text-white py-16 text-center rounded-lg">
+            <section className="bg-purple-600 h-[50vh] relative text-white py-16 text-center rounded-lg">
                 <div className='w-[70%] mx-auto'>
                     <h2 className="text-3xl md:text-4xl font-bold mb-4">
                         Upgrade Your Tech Accessories with Gadget Heaven Accessories
@@ -14,6 +14,13 @@ const Banner = () => {
                     <button className="bg-white text-purple-600 font-semibold px-6 py-3 rounded-full shadow-lg hover:bg-purple-100 transition duration-300">
                         Shop Now
                     </button>
+                </div>
+            </section>
+            <section className='absolute-section absolute -mt-36 mb-60'>
+                <div className='border-2 border-white ml-[36vh] rounded-lg'>
+                    <div className='w-[100%] mx-auto flex p-3'>
+                        <img src="/public/assets/banner.jpg" alt="banner" className='h-[35vh] w-full] rounded-lg mb-60'/>
+                    </div>
                 </div>
             </section>
         </div>
